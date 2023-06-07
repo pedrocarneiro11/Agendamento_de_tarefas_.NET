577a402c2703da396f61b49fca9b286f4606097a
Pedro Carneiro <pedrocarneirocunha87@gmail.com>
2023-06-07 15:26:03 -0300

    Construtor da classe tarefa

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -7,6 +7,14 @@ namespace _pasta_projeto.Models
 {
     public class Tarefa
     {
-        
+        public int Id{ get; set; }
+
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data { get; set; }
+
+        public enum StatusTarefa
+        {
+        }
     }
 }
