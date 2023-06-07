66cf36487a4730c6bc276cffceee849b14d70387
Pedro Carneiro <pedrocarneirocunha87@gmail.com>
2023-06-07 15:17:43 -0300

    commit inicial

---
+++ Context/TarefaContext.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _pasta_projeto.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _pasta_projeto.Context
+{
+    public class TarefaContext : DbContext
+    {
+        
+    }
+}
---
+++ Controllers/TarefaController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace pasta_projeto.Controllers
+{
+    [Route("[controller]")]
+    public class TarefaController : Controller
+    {
+        private readonly ILogger<TarefaController> _logger;
+
+        public TarefaController(ILogger<TarefaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
