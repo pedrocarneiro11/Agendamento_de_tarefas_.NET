b90040b894515816fa6a3d5444174565d7fb7217
Pedro Carneiro <pedrocarneirocunha87@gmail.com>
2023-06-08 16:38:38 -0300

    criando metodos get e post

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -3,30 +3,62 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using _pasta_projeto.Context;
+using _pasta_projeto.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace pasta_projeto.Controllers
 {
+    [ApiController]
     [Route("[controller]")]
     public class TarefaController : Controller
     {
-        private readonly ILogger<TarefaController> _logger;
+        private readonly TarefaContext _context;
 
-        public TarefaController(ILogger<TarefaController> logger)
+        public TarefaController(TarefaContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        // TODO: CRIAR METODO POST : /Tarefa  
+
+        [HttpPost]
+        public IActionResult Create(Tarefa tarefa)
         {
-            return View();
+            _context.Add(tarefa);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetByID), new { id = tarefa.Id}, tarefa);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        // TODO: Criar método GetByID
+
+        [HttpGet("{id}")]
+        public IActionResult GetByID(int id)
         {
-            return View("Error!");
+            var tarefa = _context.Tarefas.Find(id);
+
+            if(tarefa != null)
+            {
+                int Id = id;
+                string Titulo = tarefa.Titulo;
+                string Descricao = tarefa.Descricao;
+                DateTime Data = tarefa.Data;
+
+                return Ok(tarefa);
+            }
+            else 
+            {
+                return NotFound();
+            }
         }
+
+        // TODO: CRIAR METODO GET : /Tarefa/{id}
+        // TODO: CRIAR METODO GET : /Tarefa/ObterTodos
+        // TODO: CRIAR METODO GET : /Tarefa/ObterPorTitulo
+        // TODO: CRIAR METODO GET : /Tarefa/ObterPorData
+        // TODO: CRIAR METODO GET : /Tarefa/ObterPorStatus
+        // TODO: CRIAR METODO PUT: /Tarefa/{id}
+        // TODO: CRIAR METODO DELETE: /Tarefa/{id}
     }
 }
